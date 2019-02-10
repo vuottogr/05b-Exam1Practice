2b1a62b5bce0440d6674e552fc047c2d1574c3d4
vuottogr <vuottogr@rose-hulman.edu>
2019-02-10 16:53:34 -0500

    .

--- src/problem3.py
+++ src/problem3.py
@@ -3,8 +3,8 @@ PRACTICE Exam 1, problem 3.
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
          Valerie Galluzzi, Mark Hays, Amanda Stouder, Aaron Wilkin,
-         their colleagues, and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues, and Elle Vuotto.
+"""  # done: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import rosegraphics as rg
 
@@ -96,6 +96,15 @@ def run_test_problem3a():
     print('Test 4 expected:', expected)
     print('       actual:  ', answer)
 
+
+    # Test 5 (it is on window 3):
+    point = rg.Point(30, 40)
+    expected = 218
+    answer = problem3a(window3, point, 6)
+    print()
+    print('Test 4 expected:', expected)
+    print('       actual:  ', answer)
+
     window3.close_on_mouse_click()
 
     # -------------------------------------------------------------------------
@@ -137,6 +146,15 @@ def problem3a(window, point, n):
         :type point:  rg.Point
         :type n:      int
     """
+
+
+    line = rg.Line(point, rg.Point((point.x - 50), point.y))
+    line.attach_to(window)
+
+    for k in range(n):
+        return k
+
+
     # -------------------------------------------------------------------------
     # TODO: 3. Implement and test this function.
     #   Note that you should write its TEST function first (above).
